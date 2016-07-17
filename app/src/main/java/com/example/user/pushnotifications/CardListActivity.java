6d9ad3804fc9ff7dcfceff45495b0726f1d1dc8f
ArvidQuarshie <arvidquarshie@gmail.com>
2016-07-17 19:29:53 +0300

    added picasso library and link to jpg

---
+++ app/src/main/java/com/example/user/pushnotifications/CardListActivity.java
@@ -0,0 +1,54 @@
+package com.example.user.pushnotifications;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.CardThumbnail;
+import it.gmariotti.cardslib.library.view.CardListView;
+
+/**
+ * Created by user on 7/17/16.
+ */
+class CardListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int listImages[] = new int[]{R.layout.activity_main, R.layout.secondpicture,
+                R.layout.thirdpicture ,R.layout.fourthpicture, R.layout.fifthpicture};
+
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        for (int i = 0; i<5; i++) {
+            // Create a Card
+            Card card = new Card(this);
+            // Create a CardHeader
+            CardHeader header = new CardHeader(this);
+            // Add Header to card
+            header.setTitle("Kibanda " + i);
+            card.setTitle("sample title");
+            card.addCardHeader(header);
+
+            CardThumbnail thumb = new CardThumbnail(this);
+            thumb.setDrawableResource(listImages[i]);
+            card.addCardThumbnail(thumb);
+
+            cards.add(card);
+        }
+
+        CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(this, cards);
+
+        CardListView listView = (CardListView) this.findViewById(R.id.myList);
+        if (listView != null) {
+            listView.setAdapter(mCardArrayAdapter);
+        }
+    }
+}
--- app/src/main/java/com/example/user/pushnotifications/MainActivity.java
+++ app/src/main/java/com/example/user/pushnotifications/MainActivity.java
@@ -2,15 +2,51 @@ package com.example.user.pushnotifications;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
-
+import com.squareup.picasso.Picasso;
 
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private ImageView imageView2;
+    private ImageView imageView3;
+    private ImageView imageView4;
+    private ImageView imageView5;
+    private ImageView imageView6;
+
+
+
+
+//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(this)
+                .load("http://images5.fanpop.com/image/photos/26500000/Robert-Downey-Jr-SH2-Movie-Posters-robert-downey-jr-26552473-800-1278.jpg")
+                .placeholder(R.drawable.placeholder)   // optional
+                .error(R.drawable.error)      // optional
+                .resize(250,250)                        // optional
+                .into(imageView);
+        imageView = (ImageView) findViewById(R.id.imageView2);
+        Picasso.with(this)
+                .load("https://s-media-cache-ak0.pinimg.com/originals/34/5a/0d/345a0d25e5968310a091adbe5955263a.jpg")
+                .placeholder(R.drawable.placeholder)   // optional
+                .error(R.drawable.error)      // optional
+                .resize(250,250)                        // optional
+                .into(imageView);
+        imageView = (ImageView) findViewById(R.id.imageView3);
+        Picasso.with(this)
+                .load("https://s-media-cache-ak0.pinimg.com/236x/41/2f/96/412f96d1d0a926436fc4cc2c755fac18.jpg")
+                .placeholder(R.drawable.placeholder)   // optional
+                .error(R.drawable.error)      // optional
+                .resize(250,250)                        // optional
+                .into(imageView);
+
     }
+
+
 }
